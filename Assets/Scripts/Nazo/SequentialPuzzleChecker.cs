bf7553b6920dc4aa85e8d9a3f1918ddd45159f9e
CHARA\meich <mei.charaa@gmail.com>
2023-04-08 12:18:31 +0900

    [add]ナゾのテンプレートプレハブを作りたいって思った

---
+++ Assets/Scripts/Nazo/SequentialPuzzleChecker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>決められた順番に特定の行動をすればクリアとなるナゾの判定をするクラス</summary>
+public class SequentialPuzzleChecker : MonoBehaviour
+{
+    [SerializeField] private int _puzzleCount = 5;
+    [SerializeField] private GameObject[] _lights = new GameObject[5];
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Correct()
+    {
+
+    }
+}
